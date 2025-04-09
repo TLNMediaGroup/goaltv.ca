1f6fa005c8e043c6cb20e8ae1f335d8a73c22490
Andrew Debyl <andrewd@tlnmediagroup.com>
2025-04-09 16:59:10 -0400

    footer draft

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import "../styles/footerStyles.css";
 import goalBimbim from "../assets/Footer/goal-bimbim@2x.png";
 import goalCanadaWorld from "../assets/Footer/goal-canada-world@2x.png";
 import goalCineLatino from "../assets/Footer/goal-cine-latino@2x.png";
-import goalEuroworld from "../assets/Footer/goal-euroworld.png";
 import goalEuroworldv1 from "../assets/Footer/goal-euroworldv1.png";
 import goalMediaset from "../assets/Footer/goal-mediaset@2x.png";
 import goalNinos from "../assets/Footer/goal-ninos@2x.png";
@@ -18,38 +17,89 @@ import goalVivatv from "../assets/Footer/goal-vivatv.png";
 
 function Footer() {
   const imageUrls = [
-    goalBimbim,
-    goalCanadaWorld,
-    goalCineLatino,
-    goalEuroworld,
-    goalEuroworldv1,
-    goalMediaset,
-    goalNinos,
-    goalSit,
-    goalTgcom24,
-    goalTin,
-    goalTinSudios,
-    goalTinTop10,
-    goalTmg,
-    goalUnivision,
-    goalVivatv,
+    {
+      img: goalBimbim,
+      url: "https://telebimbi.ca/",
+    },
+    {
+      img: goalCanadaWorld,
+      url: "https://canadaworld.tv/",
+    },
+    {
+      img: goalCineLatino,
+      url: "https://www.tln.ca/cinelatino/",
+    },
+    {
+      img: goalEuroworldv1,
+      url: "/",
+    },
+    {
+      img: goalMediaset,
+      url: "https://mediasetitalia.ca/",
+    },
+    {
+      img: goalNinos,
+      url: "https://teleninos.ca/",
+    },
+    {
+      img: goalSit,
+      url: "https://salsaintoronto.com/",
+    },
+    {
+      img: goalTgcom24,
+      url: "https://tgcom24.ca/",
+    },
+    {
+      img: goalTin,
+      url: "https://www.tln.ca/",
+    },
+    {
+      img: goalTinSudios,
+      url: "https://www.tlnoriginals.com/",
+    },
+    {
+      img: goalTinTop10,
+      url: "https://www.tln.ca/top10/",
+    },
+    {
+      img: goalTmg,
+      url: "https://www.tlnmediagroup.ca/",
+    },
+    {
+      img: goalUnivision,
+      url: "https://univision.ca/",
+    },
+    {
+      img: goalVivatv,
+      url: "https://landing.vivatv.ca/",
+    },
   ];
 
   return (
-    <div>
+    <div className="footer">
       <div className="footer-grid-container">
         <div className="footer-grid-container-top">
           <div className="footer-logo-wrapper">
-            {imageUrls.map((img, index) => (
-              <a href="https://www.tlnmediagroup.ca/">
-                <img src={img} className="footer-logo" alt="" key={index} />
+            {imageUrls.map((item, index) => (
+              <a href={item.url} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={item.img}
+                  className="footer-logo"
+                  alt=""
+                  key={index}
+                />
               </a>
             ))}
           </div>
           <div className="footer-logo-wrapper mobile-footer">
-            {imageUrls.map((img, index) => (
-              <a href="https://www.tlnmediagroup.ca/">
-                <img src={img} className="footer-logo" alt="" key={index} />
+            {imageUrls.map((item, index) => (
+              <a href={item.url} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={item.img}
+                  className="footer-logo"
+                  alt=""
+                  key={index}
+                />
               </a>
             ))}
           </div>
@@ -57,29 +107,40 @@ function Footer() {
         <div className="footer-grid-container-bottom">
           <div className="footer-wrapper">
             <div className="footer-column">
-              <h2 className="footer-dropdown">
-                <span>EUROWORLDSPORT</span>
-                <span className="footer-dropdown-icon">+</span>
-              </h2>
-              <div className="footer-dropdown-menu">
-                <a href="/">SUBSCRIBE TODAY</a>
-                <a href="/">ROGERS | CHANNEL 425</a>
-                <a href="/">WWW.ROGERS.COM</a>
-                <a href="/">1 (888) 764-3771</a>
+              <h2 className="footer-dropdown">EUROWORLDSPORT</h2>
+              <div className="location">
+                <div>
+                  <a href="/">SUBSCRIBE TODAY</a>
+                </div>
+                <div>
+                  <a href="/">ROGERS | CHANNEL 425</a>
+                </div>
+                <div>
+                  <a href="/">WWW.ROGERS.COM</a>
+                </div>
+                <div>
+                  <a href="/">1 (888) 764-3771</a>
+                </div>
               </div>
             </div>
             <div className="footer-column">
               <h2 className="footer-dropdown">CONTACT US</h2>
-              <p className="location">
-                COLU<br />
-                COLUMBUS CENTRE, LEVEL 2 <br />
-                901 LAWRENCE AVE WEST
-                <br />
-                TORONTO, ON, M6A 1C3 <br />
-                <a href="mailto:info@tlnmediagroup.com">
-                  INFO@TLNMEDIAGROUP.COM
-                </a>
-              </p>
+              <div className="location">
+                <div>
+                  <p>COLUMBUS CENTRE, LEVEL 2</p>
+                </div>
+                <div>
+                  <p>901 LAWRENCE AVE WEST</p>
+                </div>
+                <div>
+                  <p>TORONTO, ON, M6A 1C3</p>
+                </div>
+                <div>
+                  <a href="mailto:info@tlnmediagroup.com">
+                    INFO@TLNMEDIAGROUP.COM
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
